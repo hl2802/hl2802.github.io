7ad0c44092cc12a261a5f52e96e6aeb8215d3514
NGUYỄN THANH HƯƠNG <93818052+hl2802@users.noreply.github.com>
2022-11-12 22:52:26 +0700

    upload

---
+++ buttonClick-master/buttonClick/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("buttonClick")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("buttonClick")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06bdc643-035a-43e5-98b3-20c148d0e5f8")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
